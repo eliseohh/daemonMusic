fefcb5c73a308a718f0d32fec92b75f6bcc54ebd
Elise H <eliseohh@gmail.com>
2021-04-11 18:15:37 -0400

    removed overused mapper

--- src/main/java/com/daemongear/presentation/controller/MusicController.java
+++ src/main/java/com/daemongear/presentation/controller/MusicController.java
@@ -1,12 +1,12 @@
 package com.daemongear.presentation.controller;
 
 import com.daemongear.core.domain.service.UrlService;
-import com.daemongear.core.domain.entity.UrlBody;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@RequestMapping("/download/music")
-@RestController
+@RestController("/download/music")
 public class MusicController {
 
     private final UrlService urlService;
